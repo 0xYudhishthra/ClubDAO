0382d22db6098f6581a3bfd84a903f67f78f3135
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-08-12 19:44:54 +0800

    feat: fix card

--- components/FrostedCard/index.js
+++ components/FrostedCard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FrostedGlassBox = ({ children, className }) => {
   return (
-    <div className={`box my-4 shadow-lg w-96 h-48 rounded-lg backdrop-blur-md backdrop-brightness-150 bg-opacity-20 bg-white p-4 ${className}`}>
+    <div className={`box my-4 shadow-lg w-96 h-48 rounded-lg backdrop-blur-md backdrop-brightness-150 bg-opacity-0 bg-white p-4 ${className}`}>
       {children}
     </div>
   );
