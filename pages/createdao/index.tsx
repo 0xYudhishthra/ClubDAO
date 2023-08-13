8386393ec0652431abcae05fb9ebfc36e9fa7250
cheongyeechian <cheongyeechian@gmail.com>
2023-08-13 17:06:38 +0800

    feat:inherit FrostedGlass component

--- pages/createdao/index.tsx
+++ pages/createdao/index.tsx
@@ -3,6 +3,8 @@ import type { NextPage } from "next";
 import { useAccount, useBalance } from "wagmi";
 import { Button, Layout, Loader, WalletOptionsModal } from "../../components";
 import { useRouter } from "next/router";
+import FrostedGlassBox from "../../components/FrostedCard";
+
 
 
 const CreateDAO = () => {
@@ -21,7 +23,7 @@ const CreateDAO = () => {
         return (
             <>
                 <div className="create-container ">
-                    <div className="create select-blockchain-card">
+                    {/* <div className="create select-blockchain-card">
                         <div className="card-title mb-8 font-bold">Select Blockchain</div>
                         <div className="list-choice">
                             <div className="list-choice-title">Testnet</div>
@@ -50,7 +52,31 @@ const CreateDAO = () => {
                         <div className="card-body">
                         This section is where you can articulate the essence of your DAO. Outline the purpose, values, and goals that define your decentralized autonomous organization, guiding potential contributors in understanding its unique identity.</div>
                         <div> </div>
-                    </div>
+                    </div> */}
+                    <FrostedGlassBox className={"h-auto"}>
+                    <div className="card-title mb-8 font-bold">Select Blockchain</div>
+                        <div className="list-choice">
+                            <div className="list-choice-title">Testnet</div>
+                                <div className="list-choice-objects">
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Optimism Goerli</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Base Goerli</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Zora Testnet</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Mode Testnet</span>
+                                    </label>
+                                </div>
+                        </div>
+                    </FrostedGlassBox>
                 </div>
             </>
         );
