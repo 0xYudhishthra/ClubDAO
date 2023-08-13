ec42cc1846ab0cabcf3b691679877a921b96291b
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-08-13 18:26:11 +0800

    feat: bounties page done styling

--- pages/bounties/index.tsx
+++ pages/bounties/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useAccount, useBalance } from "wagmi";
+import { Button, Layout, Loader, WalletOptionsModal } from "../../components";
+import { useRouter } from "next/router";
 
 export default function HackathonBountiesDashboard() {
+
+    const [showWalletOptions, setShowWalletOptions] = useState(false);
+    const [{ data: accountData, loading: accountLoading }] = useAccount();
+    const [{ data: balanceData, loading: balanceLoading }] = useBalance({
+      addressOrName: accountData?.address,
+      watch: true,
+    });
+
     const hackathonBounties = [
         { id: 1, title: 'Decentralized Voting System', sponsor: 'Ethereum Foundation', reward: '10 ETH', status: 'Open' },
         { id: 2, title: 'Cross-chain Asset Bridge', sponsor: 'Polygon', reward: '8 ETH', status: 'In Progress' },
@@ -9,8 +22,11 @@ export default function HackathonBountiesDashboard() {
     ];
 
     return (
-        <div className="hackathon-bounties-dashboard flex flex-col items-center gap-20 p-20">
-            <h1 className="text-2xl font-bold">Hackathon Bounties</h1>
+        <>
+        {/* <WalletOptionsModal open={showWalletOptions} setOpen={setShowWalletOptions}/> */}
+        <Layout showWalletOptions={showWalletOptions} setShowWalletOptions={setShowWalletOptions} children={undefined} />
+        <div className="hackathon-bounties-dashboard flex flex-col items-center gap-20 p-20 mt-30">
+            <h1 className="text-2xl font-bold mt-20">Hackathon Bounties</h1>
 
             <div className="bounties-list w-full max-w-md">
                 {hackathonBounties.map(bounty => (
@@ -29,5 +45,6 @@ export default function HackathonBountiesDashboard() {
                 </button>
             </Link>
         </div>
+        </>
     );
 }
