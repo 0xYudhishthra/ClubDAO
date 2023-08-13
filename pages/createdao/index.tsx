b4be74ed8e278350bd3cb73b0ade950e80bba69c
cheongyeechian <cheongyeechian@gmail.com>
2023-08-13 16:57:06 +0800

    feat:createdao card create

--- pages/createdao/index.tsx
+++ pages/createdao/index.tsx
@@ -4,51 +4,74 @@ import { useAccount, useBalance } from "wagmi";
 import { Button, Layout, Loader, WalletOptionsModal } from "../../components";
 import { useRouter } from "next/router";
 
+
 const CreateDAO = () => {
-const [showWalletOptions, setShowWalletOptions] = useState(false);
+    const [showWalletOptions, setShowWalletOptions] = useState(false);
     const [{ data: accountData, loading: accountLoading }] = useAccount();
     const [{ data: balanceData, loading: balanceLoading }] = useBalance({
         addressOrName: accountData?.address,
         watch: true,
     });
-    
 
     const loading = (accountLoading || balanceLoading) && !balanceData;
+    const router = useRouter();
+
 
     const renderContent = () => {
         return (
             <>
-            <div className="cards-container">
-                <div className="card create-dao-card">
-                <div className="card-title">Select Blockchain</div>
-                <div className="card-body">
-                    Effortlessly mint tokens, configure governance parameters, and deploy your DAO on the blockchain within minutes</div>
-                </div>
-                <div className="card view-dao-card">
-                    <div className="card-title">View DAO</div>
-                    <div className="card-body">
-                    Explore DAO activities, analyze governance decisions, and monitor blockchain-based operations effortlessly</div>
-                </div>
+                <div className="create-container ">
+                    <div className="create select-blockchain-card">
+                        <div className="card-title mb-8 font-bold">Select Blockchain</div>
+                        <div className="list-choice">
+                            <div className="list-choice-title">Testnet</div>
+                                <div className="list-choice-objects">
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Optimism Goerli</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Base Goerli</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Zora Testnet</span>
+                                    </label>
+                                    <label>
+                                        <input type="radio" name="testnet" />
+                                        <span>Mode Testnet</span>
+                                    </label>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="create">
+                        <div className="card-title font-bold mb-8 ">Describe your DAO</div>
+                        <div className="card-body">
+                        This section is where you can articulate the essence of your DAO. Outline the purpose, values, and goals that define your decentralized autonomous organization, guiding potential contributors in understanding its unique identity.</div>
+                        <div> </div>
+                    </div>
                 </div>
             </>
         );
     };
+
     return (
         <>
-          <WalletOptionsModal
-            open={showWalletOptions}
-            setOpen={setShowWalletOptions}
-          />
-          {/* Header */}
-          <Layout
-            showWalletOptions={showWalletOptions}
-            setShowWalletOptions={setShowWalletOptions}
-          >
-            <div className="grid h-screen place-items-center">
-              <div className="grid place-items-center">{renderContent()}</div>
-            </div>
-          </Layout>
+            <WalletOptionsModal
+                open={showWalletOptions}
+                setOpen={setShowWalletOptions}
+            />
+            <Layout
+                showWalletOptions={showWalletOptions}
+                setShowWalletOptions={setShowWalletOptions}
+            >
+                <div className="grid h-screen place-items-center">
+                    <div className="grid place-items-center">{renderContent()}</div>
+                </div>
+            </Layout>
         </>
-      );
+    );
 };
+
 export default CreateDAO;
